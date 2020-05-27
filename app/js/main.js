bdde54a4301da9558a9619ad963347b1d74d9ca7
oli4ka-dev <oli4kavelikorod@gmail.com>
2020-05-27 15:47:46 +0300

    added page with adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,84 @@
-$(function(){
-$('.slide__inner').slick({
-    arrows: false,
-    dots: true,
-});
-    
+$(function () {
+    $('.slide__inner').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        speed: 1000,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    $('.blog__inner').slick({
+        arrows: true,
+        adaptiveHeight: true,
+        speed: 1000,
+        easing: 'ease',
+        infinite: false,
+        waitForAnimate: false,
+        responsive: [
+            {
+              breakpoint: 800,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+            },
+          ]
+    });
+
+    $('select').styler();
+
+  // Бургер меню
+        $('.menu__btn').on('click', function(){
+            $(this).toggleClass('open');
+            $('.menu__list').slideToggle();
+        });
+  
+// Кнопка для @media (max-width: 380px)
+
+        $('.header__btn-menu ').on('click', function(){
+            $('.header__box').toggleClass('active');
+        });
+
+    // Плавный скрол к якорю
+
+
+    //var margin = 100; // переменная для контроля докрутки
+    $("a.scrollto").click(function () { // тут пишите условия, для всех ссылок или для конкретных
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top - 120 // .top+margin - ставьте минус, если хотите увеличить отступ
+        }, {
+            duration: 900, // тут можно контролировать скорость
+            easing: "swing",
+        });
+        return false;
+    });
+
+
+    // new WOW().init();
+   
+
+    AOS.init();
+
+   new SimpleBar  (document.getElementById ('myEl'), {
+       autoHide: false,
+       scrollbarMinSize: 20
+   }) ;
+   new SimpleBar  (document.getElementById ('myEl2'), {
+    autoHide: false,
+    scrollbarMinSize: 20
+}) ;
+new SimpleBar  (document.getElementById ('myEl3'), {
+    autoHide: false,
+    scrollbarMinSize: 20
+}) ;
+
+
+
+    var mixer = mixitup('.products__inner-box', {
+        load: {
+            filter: '.category-complex'
+        }
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,12 @@ let gulp = require('gulp'),
             'node_modules/slick-carousel/slick/slick.css',
             'node_modules/magnific-popup/dist/magnific-popup.css',
             'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
+            'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+            'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
+            'node_modules/simplebar/dist/simplebar.css',           
+            'node_modules/animate.css/animate.css',
+            'node_modules/aos/dist/aos.css',
+           
         ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -35,6 +41,12 @@ let gulp = require('gulp'),
                 'node_modules/slick-carousel/slick/slick.js',
                 'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
                 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+                'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+                'node_modules/mixitup/dist/mixitup.js',
+                'node_modules/simplebar/dist/simplebar.js',
+                'node_modules/wow.js/dist/wow.js',
+                'node_modules/aos/dist/aos.js',
+               
             ])
             .pipe(concat('libs.min.js'))
             .pipe(uglify())
